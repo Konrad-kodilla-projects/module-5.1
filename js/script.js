9df3b8d17350e333a1fe6e529853b3de962c28f6
Konrad <konrad.bonder@gmail.com>
2019-09-18 16:02:50 +0200

    Add getMoveName and displayResults funtions

--- js/script.js
+++ js/script.js
@@ -1,40 +1,58 @@
-/* eslint-disable no-undef */
+var argMoveId, PlayerMove, ComputerMove, computerMove, playerMove, randomNumber, playerInput;
 
-// Czemu deklaracja zmiennej i przypisanie wartości odbywa się w dwóch liniach?
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+function getMoveName(argMoveId) {
+    console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+    if (argMoveId == 1) {
+        return 'kamień';
+    } else if (argMoveId == 2) {
+        return 'papier';
+    } else if (argMoveId == 3) {
+        return 'nożyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
+        return 'kamień';
+    }
+}
 
-var playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! i wygrałem');
+const RESULTS = [
+    ['papier', 'kamień', 1],
+    ['papier', 'nożyce', 0],
+    ['kamień', 'papier', 0],
+    ['kamień', 'nożyce', 1],
+    ['nożyce', 'papier', 1],
+    ['nożyce', 'kamień', 0]
+];
+
+function displayResult(RESULTS, PlayerMove, ComputerMove) {
+    printMessage('Gracz zagrywa: ' + PlayerMove + ', a Komputer: ' + ComputerMove)
+
+    if (PlayerMove == ComputerMove){
+        printMessage('Remis!')
+    } else {
+        for (var i = 0; i < RESULTS.length; i++) {
+            console.log('odpalam pętlę i sprawdzam RESULT = ' + RESULTS[i]);
+            if (PlayerMove == RESULTS[i][0] && ComputerMove == RESULTS[i][1]) {
+                if (RESULTS[i][2] == 1) {
+                    printMessage('Wygrałeś!');
+                    break;
+                } else {
+                    printMessage('Przegrałeś :(');
+                    break;
+                }
+            }
+        }
+    }
+
+}
 
 
-var randomNumber;
+
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('wybór ruchu gracza to: ' + playerInput);
+playerMove = getMoveName(playerInput);
+console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-// var randomNumber11_19 = Math.floor(Math.random() * 10 + 10);
-// printMessage('Losowa liczba z zakresu 11-19 to: ' + randomNumber11_19);
-
-if (randomNumber == 1) {
-    computerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier'
-} else if (randomNumber == 3) {
-    computerMove = 'nożyce'
-}
-console.log('Komputer wyrzuca: ' + computerMove);
-
-var playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier'
-} else if (playerInput == '3') {
-    playerMove = 'nożyce'
-} else {
-    playerMove = 'nieznany ruch';
-}
-console.log('Gracz wybiera: ' + playerMove);
+console.log('wylosowana liczba to: ' + randomNumber);
+computerMove = getMoveName(randomNumber);
+console.log('ruch komputera to: ' + computerMove);
+displayResult(RESULTS, playerMove, computerMove);
