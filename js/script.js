05254b97a8d31ea9c54bc3ea8c4936a123c6e6c4
Konrad <konrad.bonder@gmail.com>
2019-09-18 15:02:36 +0200

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-// Czemu stworzenie zmiennej i przypisanie wartości odbywa się w dwóch liniach?
+// Czemu deklaracja zmiennej i przypisanie wartości odbywa się w dwóch liniach?
 var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
@@ -23,5 +23,18 @@ if (randomNumber == 1) {
 } else if (randomNumber == 3) {
     computerMove = 'nożyce'
 }
-
 console.log('Komputer wyrzuca: ' + computerMove);
+
+var playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier'
+} else if (playerInput == '3') {
+    playerMove = 'nożyce'
+} else {
+    playerMove = 'nieznany ruch';
+}
+console.log('Gracz wybiera: ' + playerMove);
