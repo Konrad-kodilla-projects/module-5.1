a6a7b45af25e343bb9d224f87b158aa0239a74ac
Konrad <konrad.bonder@gmail.com>
2019-09-18 13:44:29 +0200

    Add script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Czemu stworzenie zmiennej i przypisanie wartości odbywa się w dwóch liniach?
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! i wygrałem')
